45711bdf17a4dadb4edd921b56f3aceb3612ad7b
Loka <loka@iim.ai>
2023-09-27 19:10:03 +0800

    删除无关代码

--- src/api/emoticon.js
+++ src/api/emoticon.js
@@ -5,16 +5,6 @@ export const ServeFindUserEmoticon = () => {
   return get('/api/v1/emoticon/list')
 }
 
-// 查询系统表情包服务接口
-export const ServeFindSysEmoticon = () => {
-  return get('/api/v1/emoticon/system/list')
-}
-
-// 设置用户表情包服务接口
-export const ServeSetUserEmoticon = data => {
-  return post('/api/v1/emoticon/system/install', data)
-}
-
 // 移除收藏表情包服务接口
 export const ServeDelCollectEmoticon = data => {
   return post('/api/v1/emoticon/del-collect-emoticon', data)
